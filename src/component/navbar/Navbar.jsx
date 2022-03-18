27fab55b7545854d57f8a922bef28a52a1380ea7
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-18 10:51:57 +0600

    Project Init

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="image">
+      <img
+        src="https://img-b.udemycdn.com/notices/web_banner/slide_1_image_udlite/4934d825-1d10-48c7-ba4e-247ce45a1656.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Navbar;
